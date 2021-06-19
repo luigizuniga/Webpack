fa2ecb1b0c1634df2dde4562eaa369dd59997b5a
luigizuniga <luigialbertozuniga@gmail.com>
2021-06-19 18:47:32 -0400

    Webpack - 09 netlify add acount

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import '@styles/main.css';
 // import '@styles/vars.styl';
 
 (async function App() {
+  console.log('Weolcome to the jungle')
   const main = null || document.getElementById('main');
   main.innerHTML = await Template();
 })();
